af986f8a8bd12be4cd614a0a70223e378d622790
Velizar Todorov <willking66@gmail.com>
2023-04-08 23:54:02 +0200

    cleanup application

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,51 +1,11 @@
-import React from 'react';
-import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import './App.css';
-import Profile from './Profile';
+import Header from './components/Header/Header';
 window.React = React
 
 export default function App() {
   return (
-    <>
-      <header className="header">
-        <Table responsive borderless>
-          <thead></thead>
-          <tbody>
-            <tr>
-              <td rowSpan={4}><ProfilePicture /></td>
-              <td><h1>{Profile.name}</h1></td>
-              <td><h2>{Profile.birthday}</h2></td>
-              <td><h2>{Profile.drivingLicense}</h2></td>
-            </tr>
-            <tr>
-              <td><h2>{Profile.email}</h2></td>
-              <td><h2>{Profile.phone}</h2></td>
-              <td><h2>{Profile.residense}</h2></td>
-            </tr>
-            <tr>
-              <td><h2>{Profile.linkedIn}</h2></td>
-              <td><h2>{Profile.gitHub}</h2></td>
-              <td><h2>{Profile.blog}</h2></td>
-            </tr>
-            <tr>
-              <td><h2>{Profile.languages.english}</h2></td>
-              <td><h2>{Profile.languages.dutch}</h2></td>
-              <td><h2>{Profile.languages.bulgarian}</h2></td>
-            </tr>
-          </tbody>
-        </Table>
-      </header>
-    </>
+    <Header />
   );
-}
-
-export function ProfilePicture() {
-  return <img
-    className="avatar"
-    src={Profile.imageUrl}
-    alt={'Photo of ' + Profile.name}
-    style={{
-      width: Profile.imageSize,
-    }}></img>
 }
--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,12 +1,53 @@
-import React, { FC } from 'react';
-import { HeaderWrapper } from './Header.styled';
 
-interface HeaderProps {}
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import './Header.css';
+import Profile from './Profile';
+window.React = React
 
-const Header: FC<HeaderProps> = () => (
- <HeaderWrapper>
-    Header Component
- </HeaderWrapper>
-);
+const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <Table responsive borderless>
+                    <thead></thead>
+                    <tbody>
+                        <tr>
+                            <td rowSpan={4}><ProfilePicture /></td>
+                            <td><h1>{Profile.name}</h1></td>
+                            <td><h2>{Profile.birthday}</h2></td>
+                            <td><h2>{Profile.drivingLicense}</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h2>{Profile.email}</h2></td>
+                            <td><h2>{Profile.phone}</h2></td>
+                            <td><h2>{Profile.residense}</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h2>{Profile.linkedIn}</h2></td>
+                            <td><h2>{Profile.gitHub}</h2></td>
+                            <td><h2>{Profile.blog}</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h2>{Profile.languages.english}</h2></td>
+                            <td><h2>{Profile.languages.dutch}</h2></td>
+                            <td><h2>{Profile.languages.bulgarian}</h2></td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </header>
+        </>
+    );
+}
+
+export function ProfilePicture() {
+    return <img
+        className="avatar"
+        src={Profile.imageUrl}
+        alt={'Photo of ' + Profile.name}
+        style={{
+            width: Profile.imageSize,
+        }}></img>
+}
 
 export default Header;
