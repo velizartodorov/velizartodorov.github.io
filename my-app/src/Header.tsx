7e2917a1f96f7b128972a784859616178bc09c41
Velizar Todorov <willking66@gmail.com>
2023-04-08 23:19:46 +0200

    add bootstrap CSS

--- my-app/src/Header.tsx
+++ my-app/src/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Profile from './Profile';
 window.React = React
@@ -8,28 +9,31 @@ export default function Header() {
   return (
     <>
       <header className="header">
-        <Table responsive>
-          <tr>
-            <td rowSpan={4}><ProfilePicture /></td>
-            <td><h1>{Profile.name}</h1></td>
-            <td><h2>{Profile.birthday}</h2></td>
-            <td><h2>{Profile.drivingLicense}</h2></td>
-          </tr>
-          <tr>
-            <td><h2>{Profile.email}</h2></td>
-            <td><h2>{Profile.phone}</h2></td>
-            <td><h2>{Profile.residense}</h2></td>
-          </tr>
-          <tr>
-            <td><h2>{Profile.linkedIn}</h2></td>
-            <td><h2>{Profile.gitHub}</h2></td>
-            <td><h2>{Profile.blog}</h2></td>
-          </tr>
-          <tr>
-            <td><h2>{Profile.languages.english}</h2></td>
-            <td><h2>{Profile.languages.dutch}</h2></td>
-            <td><h2>{Profile.languages.bulgarian}</h2></td>
-          </tr>
+        <Table responsive borderless>
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td rowSpan={4}><ProfilePicture /></td>
+              <td><h1>{Profile.name}</h1></td>
+              <td><h2>{Profile.birthday}</h2></td>
+              <td><h2>{Profile.drivingLicense}</h2></td>
+            </tr>
+            <tr>
+              <td><h2>{Profile.email}</h2></td>
+              <td><h2>{Profile.phone}</h2></td>
+              <td><h2>{Profile.residense}</h2></td>
+            </tr>
+            <tr>
+              <td><h2>{Profile.linkedIn}</h2></td>
+              <td><h2>{Profile.gitHub}</h2></td>
+              <td><h2>{Profile.blog}</h2></td>
+            </tr>
+            <tr>
+              <td><h2>{Profile.languages.english}</h2></td>
+              <td><h2>{Profile.languages.dutch}</h2></td>
+              <td><h2>{Profile.languages.bulgarian}</h2></td>
+            </tr>
+          </tbody>
         </Table>
       </header>
     </>
@@ -43,6 +47,5 @@ export function ProfilePicture() {
     alt={'Photo of ' + Profile.name}
     style={{
       width: Profile.imageSize,
-      height: Profile.imageSize
     }}></img>
 }
