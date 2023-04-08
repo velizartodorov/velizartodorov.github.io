b7cd0ca2a7faf6bfe19ea7decf77e2121ab3aca6
Velizar Todorov <willking66@gmail.com>
2023-04-08 21:44:22 +0200

    add profile in external file

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,26 +3,37 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import './App.css';
+import Profile from './Profile';
 window.React = React
 
-
-const profile = {
-  name: 'Velizar Todorov',
-  imageUrl: '/velizar-photo.jpg',
-  email: 'veltodorov@outlook.com',
-  imageSize: 200,
-};
-
-export default function Profile() {
+export default function Header() {
   return (
     <>
       <header className="header">
         <Container>
           <Row>
-            <Col>
-             <ProfilePicture></ProfilePicture>
+            <Col> <h1>{Profile.name}</h1>
             </Col>
-            <Col> <h1>{profile.name}</h1></Col>
+            <Col> <h1>{Profile.birthday}</h1>
+            </Col>
+            <Col> <h1>{Profile.drivingLicense}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col> <h1>{Profile.email}</h1>
+            </Col>
+            <Col> <h1>{Profile.phone}</h1>
+            </Col>
+            <Col> <h1>{Profile.residense}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col> <h1>{Profile.linkedIn}</h1>
+            </Col>
+            <Col> <h1>{Profile.gitHub}</h1>
+            </Col>
+            <Col> <h1>{Profile.blog}</h1>
+            </Col>
           </Row>
         </Container>
       </header>
@@ -33,10 +44,10 @@ export default function Profile() {
 export function ProfilePicture() {
   return <img
     className="avatar"
-    src={profile.imageUrl}
-    alt={'Photo of ' + profile.name}
+    src={Profile.imageUrl}
+    alt={'Photo of ' + Profile.name}
     style={{
-      width: profile.imageSize,
-      height: profile.imageSize
+      width: Profile.imageSize,
+      height: Profile.imageSize
     }}></img>
 }
---
+++ my-app/src/Profile.ts
@@ -0,0 +1,15 @@
+const Profile = {
+    name: 'Velizar Todorov',
+    imageUrl: '/velizar-photo.jpg',
+    email: 'veltodorov@outlook.com',
+    phone: '+32487371027',
+    birthday: '25 July, 1994',
+    residense: 'Gent, Belgium',
+    drivingLicense: 'Driving license type B',
+    linkedIn: 'LinkedIn',
+    gitHub: 'velizartodorov',
+    blog: 'Personal blog',
+    imageSize: 200,
+};
+
+export default Profile;
