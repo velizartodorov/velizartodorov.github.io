1233c05a88f8dac18cf8ef243fb10aa80d566066
Velizar Todorov <willking66@gmail.com>
2023-04-09 01:13:44 +0200

    refactor header

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 
+import { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import './Header.css';
 import Profile from './Profile';
@@ -6,41 +7,48 @@ import Profile from './Profile';
 const profile = new Profile();
 
 const Header = () => {
-    return (
-        <>
-            <header className="header">
-                <Table responsive borderless>
-                    <thead></thead>
-                    <tbody>
-                        <tr>
-                            <td rowSpan={4}><ProfilePicture /></td>
-                            <td><h1>{profile.name}</h1></td>
-                            <td><h2>{profile.birthday}</h2></td>
-                            <td><h2>{profile.drivingLicense}</h2></td>
-                        </tr>
-                        <tr>
-                            <td><h2>{profile.email}</h2></td>
-                            <td><h2>{profile.phone}</h2></td>
-                            <td><h2>{profile.residense}</h2></td>
-                        </tr>
-                        <tr>
-                            <td><h2>{profile.linkedIn}</h2></td>
-                            <td><h2>{profile.gitHub}</h2></td>
-                            <td><h2>{profile.blog}</h2></td>
-                        </tr>
-                        <tr>
-                            <td><h2>{profile.english}</h2></td>
-                            <td><h2>{profile.dutch}</h2></td>
-                            <td><h2>{profile.bulgarian}</h2></td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </header>
-        </>
-    );
+    setDocumentTitle();
+    return getContent();
 }
 
-export function ProfilePicture() {
+function getContent() {
+    return <header className="header">
+        <Table responsive borderless>
+            <thead></thead>
+            <tbody>
+                <tr>
+                    <td rowSpan={4}><ProfilePicture /></td>
+                    <td><h1>{profile.name}</h1></td>
+                    <td><h2>{profile.birthday}</h2></td>
+                    <td><h2>{profile.drivingLicense}</h2></td>
+                </tr>
+                <tr>
+                    <td><h2>{profile.email}</h2></td>
+                    <td><h2>{profile.phone}</h2></td>
+                    <td><h2>{profile.residense}</h2></td>
+                </tr>
+                <tr>
+                    <td><h2>{profile.linkedIn}</h2></td>
+                    <td><h2>{profile.gitHub}</h2></td>
+                    <td><h2>{profile.blog}</h2></td>
+                </tr>
+                <tr>
+                    <td><h2>{profile.english}</h2></td>
+                    <td><h2>{profile.dutch}</h2></td>
+                    <td><h2>{profile.bulgarian}</h2></td>
+                </tr>
+            </tbody>
+        </Table>
+    </header>;
+}
+
+function setDocumentTitle() {
+    useEffect(() => {
+        document.title = profile.name
+    }, []);
+}
+
+function ProfilePicture() {
     return <img
         className="avatar"
         src={profile.imageUrl}
