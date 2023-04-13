c82cae98e624b4cd06c45c8dfa20003a7b457ba2
Velizar Todorov <willking66@gmail.com>
2023-04-14 01:28:30 +0200

    add example employments

--- my-app/src/components/Employments/Employments.tsx
+++ my-app/src/components/Employments/Employments.tsx
@@ -1,32 +1,22 @@
-import { Accordion } from "react-bootstrap";
+import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { employments } from "./EmploymentsDto";
 
 const Employments = () => (
    <><h1>Employments</h1>
       <Accordion>
-         <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-               minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-               aliquip ex ea commodo consequat. Duis aute irure dolor in
-               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-               culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-         </Accordion.Item>
-         <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-               minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-               aliquip ex ea commodo consequat. Duis aute irure dolor in
-               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-               culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-         </Accordion.Item>
+         {employments.map((employment, index) => (
+            <Accordion.Item eventKey={index.toString()}>
+               <Accordion.Header>
+                  <Container fluid>
+                     <Row >
+                        <Col className="col-md-9"><h2>{employment.name}, {employment.place}</h2></Col>
+                        <Col className="col-md-auto text-right"><h4>{employment.period}</h4></Col>
+                     </Row>
+                  </Container>
+               </Accordion.Header>
+               <Accordion.Body><h4>{employment.body}</h4></Accordion.Body>
+            </Accordion.Item>
+         ))}
       </Accordion></>
 )
 
---
+++ my-app/src/components/Employments/EmploymentsDto.ts
@@ -0,0 +1,29 @@
+interface IEmployment {
+    name: string;
+    place: string;
+    period: string;
+    body: string;
+}
+
+export const employments: IEmployment[] = [
+    {
+        name: 'Software Developer at Unified Post',
+        place: 'Gent, Belgium',
+        period: 'April 2019 - February 2023',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Java Developer at ADM Solutions',
+        place: 'Gent, Belgium',
+        period: 'November 2018 - March 2019',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        name: 'Full-stack Developer at DSI Ltd. Bulgaria',
+        place: 'Rousse, Bulgaria',
+        period: 'July 2017 - May 2018',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+]
+
+export default IEmployment;
