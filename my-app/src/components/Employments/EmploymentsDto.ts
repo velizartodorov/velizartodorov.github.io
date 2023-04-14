ae323c0f64f95651d0dafe570bb1c5cd4fd43869
Velizar Todorov <willking66@gmail.com>
2023-04-14 02:33:08 +0200

    typo

--- my-app/src/components/Employments/EmploymentsDto.ts
+++ my-app/src/components/Employments/EmploymentsDto.ts
@@ -22,7 +22,7 @@ export const employments: IEmployment[] = [
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-        name: 'Full-stack Developer at DSI Ltd. Bulgaria',
+        name: 'Full-Stack Developer at DSI Ltd.',
         place: 'Rousse, Bulgaria',
         icon: '/employments/dsi-ltd-bulgaria.png',
         period: 'July 2017 - May 2018',
