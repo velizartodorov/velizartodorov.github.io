3b6e57f4ff79d480467466d0b92767baefb7a715
Velizar Todorov <willking66@gmail.com>
2023-04-23 18:12:41 +0200

    refactor header

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,45 +1,42 @@
-
 import Table from 'react-bootstrap/Table';
 import './Header.css';
 import Profile from './Profile';
 import { AddHeader, addIconTo, callTo, linkTo, mailTo, profilePicture } from './Utils';
-export const profile = new Profile();
+
+const profile = new Profile();
 
 const Header = () => {
     AddHeader(profile.name);
-    return content();
-}
-
-function content() {
-    return <header className="header">
-        <Table responsive borderless>
-            <thead></thead>
-            <tbody>
-                <tr>
-                    <td rowSpan={4}>{profilePicture(profile)}</td>
-                    <td colSpan={2}><h1>{profile.name}</h1></td>
-                    <td><h2>{addIconTo(profile.birthday)}</h2></td>
-                    <td><h2>{addIconTo(profile.drivingLicense)}</h2></td>
-                </tr>
-                <tr>
-                    <td colSpan={2}><h2>{mailTo(profile.email)}</h2></td>
-                    <td><h2>{callTo(profile.phone)}</h2></td>
-                    <td><h2>{linkTo(profile.address)}</h2></td>
-                </tr>
-                <tr>
-                    <td colSpan={2}><h2>{linkTo(profile.linkedIn)}</h2></td>
-                    <td><h2>{linkTo(profile.gitHub)}</h2></td>
-                    <td><h2>{linkTo(profile.blog)}</h2></td>
-                </tr>
-                <tr>
-                    <td><h2>{addIconTo(profile.english)}</h2></td>
-                    <td><h2>{linkTo(profile.dutch)}</h2></td>
-                    <td><h2>{linkTo(profile.german)}</h2></td>
-                    <td><h2>{addIconTo(profile.bulgarian)}</h2></td>
-                </tr>
-            </tbody>
-        </Table>
-    </header>;
-}
+    return (
+        <header className="header">
+            <Table responsive borderless>
+                <tbody>
+                    <tr>
+                        <td rowSpan={4}>{profilePicture(profile)}</td>
+                        <td colSpan={2}><h1>{profile.name}</h1></td>
+                        <td><h4>{addIconTo(profile.birthday)}</h4></td>
+                        <td><h4>{addIconTo(profile.drivingLicense)}</h4></td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><h4>{mailTo(profile.email)}</h4></td>
+                        <td><h4>{callTo(profile.phone)}</h4></td>
+                        <td><h4>{linkTo(profile.address)}</h4></td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><h3>{linkTo(profile.linkedIn)}</h3></td>
+                        <td><h4>{linkTo(profile.gitHub)}</h4></td>
+                        <td><h4>{linkTo(profile.blog)}</h4></td>
+                    </tr>
+                    <tr>
+                        <td><h4>{addIconTo(profile.english)}</h4></td>
+                        <td><h4>{linkTo(profile.dutch)}</h4></td>
+                        <td><h4>{linkTo(profile.german)}</h4></td>
+                        <td><h4>{addIconTo(profile.bulgarian)}</h4></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </header>
+    );
+};
 
 export default Header;
