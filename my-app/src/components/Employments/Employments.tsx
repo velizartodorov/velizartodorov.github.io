d6a6f19504ea81d8251281f360c78b1e13f6d6b4
Velizar Todorov <willking66@gmail.com>
2023-04-23 19:42:42 +0200

    refactor style

--- my-app/src/components/Employments/Employments.tsx
+++ my-app/src/components/Employments/Employments.tsx
@@ -2,29 +2,32 @@ import { Accordion, Col, Container, Row } from "react-bootstrap";
 import { employments } from "./EmploymentsDto";
 
 const Employments = () => (
-   <><h1>Employments</h1>
+   <><h2>Employments</h2>
       <Accordion>
          {employments.map((employment, index) => (
             <Accordion.Item eventKey={index.toString()}>
                <Accordion.Header>
                   <Container fluid>
                      <Row>
-                        <Col className="col-5 text-left">
-                           <h4>{employment.position} at {employment.company}</h4>
+                     <Col className="col-md-auto text-left">
+                           <img src={employment.icon}
+                              alt="company icon"
+                              style={{ width: 50 }}>
+                           </img>
                         </Col>
-                        <Col className="col-md-auto text-left">
-                           <img src={employment.icon} alt="company icon" style={{ width: 50 }}></img>
+                        <Col className="col-4 text-left">
+                        <h5>{employment.position} at {employment.company}</h5>
                         </Col>
                         <Col>
                            {employment.place}
                         </Col>
                         <Col className="col-md-auto text-right">
-                           <h4>{employment.period.period}</h4>
+                           <h5>{employment.period.period}</h5>
                         </Col>
                      </Row>
                   </Container>
                </Accordion.Header>
-               <Accordion.Body><h4>{employment.body}</h4></Accordion.Body>
+               <Accordion.Body>{employment.body}</Accordion.Body>
             </Accordion.Item>
          ))}
       </Accordion></>
--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -14,24 +14,24 @@ const Header = () => {
                     <tr>
                         <td rowSpan={4}>{profilePicture(profile)}</td>
                         <td colSpan={2}><h1>{profile.name}</h1></td>
-                        <td><h4>{addIconTo(profile.birthday)}</h4></td>
-                        <td><h4>{addIconTo(profile.drivingLicense)}</h4></td>
+                        <td><h5>{addIconTo(profile.birthday)}</h5></td>
+                        <td><h5>{addIconTo(profile.drivingLicense)}</h5></td>
                     </tr>
                     <tr>
-                        <td colSpan={2}><h4>{mailTo(profile.email)}</h4></td>
-                        <td><h4>{callTo(profile.phone)}</h4></td>
-                        <td><h4>{linkTo(profile.address)}</h4></td>
+                        <td colSpan={2}><h5>{mailTo(profile.email)}</h5></td>
+                        <td><h5>{callTo(profile.phone)}</h5></td>
+                        <td><h5>{linkTo(profile.address)}</h5></td>
                     </tr>
                     <tr>
-                        <td colSpan={2}><h3>{linkTo(profile.linkedIn)}</h3></td>
-                        <td><h4>{linkTo(profile.gitHub)}</h4></td>
-                        <td><h4>{linkTo(profile.blog)}</h4></td>
+                        <td colSpan={2}><h5>{linkTo(profile.linkedIn)}</h5></td>
+                        <td><h5>{linkTo(profile.gitHub)}</h5></td>
+                        <td><h5>{linkTo(profile.blog)}</h5></td>
                     </tr>
                     <tr>
-                        <td><h4>{addIconTo(profile.english)}</h4></td>
-                        <td><h4>{linkTo(profile.dutch)}</h4></td>
-                        <td><h4>{linkTo(profile.german)}</h4></td>
-                        <td><h4>{addIconTo(profile.bulgarian)}</h4></td>
+                        <td><h5>{addIconTo(profile.english)}</h5></td>
+                        <td><h5>{linkTo(profile.dutch)}</h5></td>
+                        <td><h5>{linkTo(profile.german)}</h5></td>
+                        <td><h5>{addIconTo(profile.bulgarian)}</h5></td>
                     </tr>
                 </tbody>
             </Table>
