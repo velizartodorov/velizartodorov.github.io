f5a15cd90ba4ac945f97d6beffc482c229d82de2
Velizar Todorov <willking66@gmail.com>
2023-04-23 21:02:55 +0200

    rename tsx in components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
-import Employments from './components/Employments/Employments';
-import Header from './components/Header/Header';
+import Employments from './components/Employments';
+import Header from './components/Header';
 window.React = React
 
 export function App() {
