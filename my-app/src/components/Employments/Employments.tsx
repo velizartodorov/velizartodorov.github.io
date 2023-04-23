3c652b05723aaccf7098faefd0155e6157847e2f
Velizar Todorov <willking66@gmail.com>
2023-04-23 19:51:34 +0200

    refactor Employments.tsx

--- my-app/src/components/Employments/Employments.tsx
+++ my-app/src/components/Employments/Employments.tsx
@@ -2,35 +2,32 @@ import { Accordion, Col, Container, Row } from "react-bootstrap";
 import { employments } from "./EmploymentsDto";
 
 const Employments = () => (
-   <><h2>Employments</h2>
-      <Accordion>
-         {employments.map((employment, index) => (
-            <Accordion.Item eventKey={index.toString()} key={index}>
-               <Accordion.Header>
-                  <Container fluid>
-                     <Row>
-                        <Col className="col-md-auto text-left">
-                           <img src={employment.icon}
-                              alt="company icon"
-                              style={{ width: 50 }}>
-                           </img>
-                        </Col>
-                        <Col className="col-4 text-left">
-                           <h5>{employment.position} at {employment.company}</h5>
-                        </Col>
-                        <Col>
-                           {employment.place}
-                        </Col>
-                        <Col className="col-md-auto text-right">
-                           <h5>{employment.period.period}</h5>
-                        </Col>
-                     </Row>
-                  </Container>
-               </Accordion.Header>
-               <Accordion.Body>{employment.body}</Accordion.Body>
-            </Accordion.Item>
-         ))}
-      </Accordion></>
-)
+  <>
+    <h2>Employments</h2>
+    <Accordion>
+      {employments.map((employment, index) => (
+        <Accordion.Item key={index} eventKey={index.toString()}>
+          <Accordion.Header>
+            <Container fluid>
+              <Row>
+                <Col className="col-md-auto text-left">
+                  <img src={employment.icon} alt="company icon" style={{ width: 50 }} />
+                </Col>
+                <Col className="col-4 text-left">
+                  <h5>{`${employment.position} at ${employment.company}`}</h5>
+                </Col>
+                <Col>{employment.place}</Col>
+                <Col className="col-md-auto text-right">
+                  <h5>{employment.period.period}</h5>
+                </Col>
+              </Row>
+            </Container>
+          </Accordion.Header>
+          <Accordion.Body>{employment.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  </>
+);
 
 export default Employments;
