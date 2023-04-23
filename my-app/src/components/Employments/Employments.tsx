2a17ecda3f93a448fa8b189de36de974e4cb2f77
Velizar Todorov <willking66@gmail.com>
2023-04-23 19:49:00 +0200

    fix error key

--- my-app/src/components/Employments/Employments.tsx
+++ my-app/src/components/Employments/Employments.tsx
@@ -5,18 +5,18 @@ const Employments = () => (
    <><h2>Employments</h2>
       <Accordion>
          {employments.map((employment, index) => (
-            <Accordion.Item eventKey={index.toString()}>
+            <Accordion.Item eventKey={index.toString()} key={index}>
                <Accordion.Header>
                   <Container fluid>
                      <Row>
-                     <Col className="col-md-auto text-left">
+                        <Col className="col-md-auto text-left">
                            <img src={employment.icon}
                               alt="company icon"
                               style={{ width: 50 }}>
                            </img>
                         </Col>
                         <Col className="col-4 text-left">
-                        <h5>{employment.position} at {employment.company}</h5>
+                           <h5>{employment.position} at {employment.company}</h5>
                         </Col>
                         <Col>
                            {employment.place}
