8b173d221b03100e1c86e46b2a669a172b6a0a8d
Velizar Todorov <willking66@gmail.com>
2023-04-23 23:02:28 +0200

    rename dsi icon

--- my-app/src/components/Employments/employments.ts
+++ my-app/src/components/Employments/employments.ts
@@ -39,7 +39,7 @@ export const employments: IEmployment[] = [
         position: "Full-Stack Developer",
         company: 'DSI Ltd.',
         place: 'Rousse, Bulgaria',
-        icon: '/employments/dsi-ltd-bulgaria.png',
+        icon: '/employments/dsi.png',
         period: new Period(new Date(2017, 7), new Date(2018, 5)),
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
