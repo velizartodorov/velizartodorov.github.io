c66507297680e85e72ba0a89195ab57852c1e98d
velizartodorov <willking66@gmail.com>
2024-01-21 00:50:09 +0100

    :art: add accordion to employments

--- my-app/src/components/employments/employments.tsx
+++ my-app/src/components/employments/employments.tsx
@@ -5,44 +5,48 @@ import { bullet, display } from "./utils";
 
 const Employments = () => (
   <>
-    <Card>
-      <Card.Header><h2>Employments 💼</h2></Card.Header>
-      <Card.Body>
-        <Accordion>
-          {employments.map((employment, index) => (
-            <Accordion.Item key={index} eventKey={index.toString()}>
-              <Accordion.Header>
-                <Container fluid>
-                  <Row className="align-items-center">
-                    <Col xs="auto" className="text-left">
-                      <img src={employment.icon} alt="company icon" style={{ width: 50 }} />
-                    </Col>
-                    <Col xs="5" className="text-left">
-                      <h5>{`${employment.position} at ${employment.company}`}</h5>
-                    </Col>
-                    <Col>{employment.place}</Col>
-                    <Col xs="auto" className="text-right">
-                      <h5>{display(employment.period)}</h5>
-                    </Col>
-                  </Row>
-                </Container>
-              </Accordion.Header>
-              <Accordion.Body>
-                {employment.body.map((bodyItem) => (
-                  <span key={uuidv4()}>{bodyItem}<br></br></span>
-                ))}
-                {employment.references.map((link) => (
-                  <div key={uuidv4()}>
-                    <span>{bullet()} </span>
-                    <a href={link.href}>{link.value}</a><br />
-                  </div>
-                ))}
-              </Accordion.Body>
-            </Accordion.Item>
-          ))}
-        </Accordion>
-      </Card.Body>
-    </Card>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Card>
+          <Accordion.Header><h2>Employments 💼</h2></Accordion.Header>
+          <Accordion.Body>
+            <Accordion>
+              {employments.map((employment, index) => (
+                <Accordion.Item key={index} eventKey={index.toString()}>
+                  <Accordion.Header>
+                    <Container fluid>
+                      <Row className="align-items-center">
+                        <Col xs="auto" className="text-left">
+                          <img src={employment.icon} alt="company icon" style={{ width: 50 }} />
+                        </Col>
+                        <Col xs="5" className="text-left">
+                          <h5>{`${employment.position} at ${employment.company}`}</h5>
+                        </Col>
+                        <Col>{employment.place}</Col>
+                        <Col xs="auto" className="text-right">
+                          <h5>{display(employment.period)}</h5>
+                        </Col>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    {employment.body.map((bodyItem) => (
+                      <span key={uuidv4()}>{bodyItem}<br></br></span>
+                    ))}
+                    {employment.references.map((link) => (
+                      <div key={uuidv4()}>
+                        <span>{bullet()} </span>
+                        <a href={link.href}>{link.value}</a><br />
+                      </div>
+                    ))}
+                  </Accordion.Body>
+                </Accordion.Item>
+              ))}
+            </Accordion>
+          </Accordion.Body>
+        </Card>
+      </Accordion.Item>
+    </Accordion>
   </>
 );
 
