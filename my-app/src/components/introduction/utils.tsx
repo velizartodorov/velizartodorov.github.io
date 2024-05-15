60de3630564ab0c737ee410d7c6750a3d991a2b4
velizartodorov <willking66@gmail.com>
2024-05-15 23:09:28 +0200

    :hammer: update `utils.tsx` in introduction

--- my-app/src/components/introduction/utils.tsx
+++ my-app/src/components/introduction/utils.tsx
@@ -1,9 +1,9 @@
 import { Period } from "../common/period";
 import { employments } from "../employments/employments.init";
 
-export const introduction = `As a software developer with ${totalWorkExperience()} years of experience in the industry, my passion lies in the Java technological stack. However, I have also gained expertise in front-end frameworks such as Angular. In addition to my daily development tasks, I prioritize following best practices, documenting project flow, extracting and translating business requirements into technical ones. Additionally, I am committed to monitoring version control systems and fostering effective team collaboration.`;
+export const introduction = `As a software developer with ${totalYears()} years of experience in the industry, my passion lies in the Java technological stack. However, I have also gained expertise in front-end frameworks such as Angular. In addition to my daily development tasks, I prioritize following best practices, documenting project flow, extracting and translating business requirements into technical ones. Additionally, I am committed to monitoring version control systems and fostering effective team collaboration.`;
 
-function totalWorkExperience(): number {
+function totalYears(): number {
     return getEmployments().reduce((totalYears, period) => {
         return totalYears + yearsDifference(period);
     }, 0);
