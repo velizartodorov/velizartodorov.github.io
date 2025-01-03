e2d94efacfdbc3e0ca335560f68ad37389dc1f73
velizartodorov <willking66@gmail.com>
2025-01-03 19:24:05 +0100

    :art: update footer

--- my-app/src/components/Footer/footer.tsx
+++ my-app/src/components/Footer/footer.tsx
@@ -2,7 +2,7 @@ import { currentYear } from './utils';
 import { profile } from './../header/profile.init';
 const Footer = () => (
   <div className="text-center mt-2 mb-2">
-    Powered by {profile.name} ® {currentYear()} 🚀
+    Proudly powered by {profile.name} ® {currentYear()} 😉🚀
   </div>
 );
 
