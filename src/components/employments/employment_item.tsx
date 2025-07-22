f22f14d66c05cdccf9d28c17cb302a6dccc7b073
velizartodorov <willking66@gmail.com>
2025-07-23 01:31:04 +0200

    :hammer: refactor employments

---
+++ src/components/employments/employment_item.tsx
@@ -0,0 +1,47 @@
+import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { v4 as uuidv4 } from 'uuid';
+import { bullet, getImageUrl } from "../common/utils";
+import { display } from "./utils";
+import { Employment } from "./employment";
+
+const EmploymentItem = ({ employment, eventKey }: { employment: Employment; eventKey: string }) => (
+  <Accordion.Item eventKey={eventKey}>
+    <Accordion.Header>
+      <Container fluid>
+        <Row className="align-items-center">
+          <Col xs="auto" className="text-left">
+            <img src={getImageUrl(employment.icon)} alt="company icon" width="30" />
+          </Col>
+          <Col xs={9} md={5} className="text-left">
+            <h5 className="employment-font">
+              {`${employment.position} at ${employment.company}`}
+            </h5>
+          </Col>
+          <Col className="d-none d-sm-block d-md-block">
+            {employment.place}
+          </Col>
+          <Col xs="auto" className="d-none d-sm-block text-right">
+            <h5 className="employment-font">
+              {display(employment.period)}
+            </h5>
+          </Col>
+        </Row>
+      </Container>
+    </Accordion.Header>
+    <Accordion.Body>
+      Company type: {employment.type} 🏢
+      <br /><br />
+      {employment.description.map((bodyItem) => (
+        <span key={uuidv4()}>{bodyItem}<br /></span>
+      ))}
+      {employment.references.map((link) => (
+        <div key={uuidv4()}>
+          <span>{bullet()} </span>
+          <a href={link.href}>{link.value}</a><br />
+        </div>
+      ))}
+    </Accordion.Body>
+  </Accordion.Item>
+);
+
+export default EmploymentItem;
--- src/components/employments/employments.tsx
+++ src/components/employments/employments.tsx
@@ -1,67 +1,25 @@
-import { Accordion, Card, Col, Container, Row } from "react-bootstrap";
-import { v4 as uuidv4 } from 'uuid';
+import { Accordion, Card } from "react-bootstrap";
 import { employments } from "./employments.init";
-import { bullet, getImageUrl } from "../common/utils";
-import { display } from "./utils";
 import './employments.css';
+import EmploymentItem from "./employment_item";
 
 const Employments = () => (
-  <>
-    <Accordion defaultActiveKey="0" className="mt-3 mx-4">
-      <Accordion.Item eventKey="0">
-        <Card>
-          <Accordion.Header>
-            <h4 className="px-2 mb-1">Employments 💼</h4>
-          </Accordion.Header>
-          <Accordion.Body>
-            <Accordion>
-              {employments.map((employment, index) => (
-                <Accordion.Item key={index} eventKey={index.toString()}>
-                  <Accordion.Header>
-                    <Container fluid>
-                      <Row className="align-items-center">
-                        <Col xs="auto" className="text-left">
-                          <img src={getImageUrl(employment.icon)}
-                            alt="company icon"
-                            width="30" />
-                        </Col>
-                        <Col xs={9} md={5} className="text-left">
-                          <h5 className="employment-font">
-                            {`${employment.position} at ${employment.company}`}
-                          </h5>
-                        </Col>
-                        <Col className="d-none d-sm-block d-md-block">
-                          {employment.place}
-                        </Col>
-                        <Col xs="auto" className="d-none d-sm-block text-right">
-                          <h5 className="employment-font">
-                            {display(employment.period)}
-                          </h5>
-                        </Col>
-                      </Row>
-                    </Container>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    Company type: {employment.type} 🏢
-                    <br></br><br></br>
-                    {employment.description.map((bodyItem) => (
-                      <span key={uuidv4()}>{bodyItem}<br></br></span>
-                    ))}
-                    {employment.references.map((link) => (
-                      <div key={uuidv4()}>
-                        <span>{bullet()} </span>
-                        <a href={link.href}>{link.value}</a><br />
-                      </div>
-                    ))}
-                  </Accordion.Body>
-                </Accordion.Item>
-              ))}
-            </Accordion>
-          </Accordion.Body>
-        </Card>
-      </Accordion.Item>
-    </Accordion>
-  </>
+  <Accordion defaultActiveKey="0" className="mt-3 mx-4">
+    <Accordion.Item eventKey="0">
+      <Card>
+        <Accordion.Header>
+          <h4 className="px-2 mb-1">Employments 💼</h4>
+        </Accordion.Header>
+        <Accordion.Body>
+          <Accordion>
+            {employments.map((employment, idx) => (
+              <EmploymentItem employment={employment} eventKey={String(idx)} key={employment.company + employment.position} />
+            ))}
+          </Accordion>
+        </Accordion.Body>
+      </Card>
+    </Accordion.Item>
+  </Accordion>
 );
 
 export default Employments;
