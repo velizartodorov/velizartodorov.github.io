60910486eb56c65b16d7129aadf284a5645081e0
velizartodorov <willking66@gmail.com>
2025-07-23 01:13:21 +0200

    :hammer: refactor education component

---
+++ src/components/education/education_item.tsx
@@ -0,0 +1,45 @@
+import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { v4 as uuidv4 } from 'uuid';
+import { display } from "./utils";
+import { bullet, getImageUrl } from "../common/utils";
+import { IEducation } from "./education.init";
+
+const EducationItem = ({ education, index }: { education: IEducation; index: number }) => (
+  <Accordion.Item eventKey={index.toString()}>
+    <Accordion.Header>
+      <Container fluid>
+        <Row className="align-items-center">
+          <Col xs="auto" className="text-left">
+            <img src={getImageUrl(education.icon)} alt="education icon" width="30" />
+          </Col>
+          <Col xs={9} md={7} className="text-left">
+            <h5 className="education-font">
+              {`${education.occupation} at ${education.institution}`}
+            </h5>
+          </Col>
+          <Col className="d-none d-sm-block">{education.place}</Col>
+          <Col xs="auto" className="text-right d-none d-sm-block">
+            <h5>{display(education.period)}</h5>
+          </Col>
+        </Row>
+      </Container>
+    </Accordion.Header>
+    <Accordion.Body>
+      {education.body.map((bodyItem) => (
+        <span key={uuidv4()}>
+          {bodyItem}
+          <br />
+        </span>
+      ))}
+      {education.references.map((link) => (
+        <div key={uuidv4()}>
+          <span>{bullet()} </span>
+          <a href={link.href}>{link.value}</a>
+          <br />
+        </div>
+      ))}
+    </Accordion.Body>
+  </Accordion.Item>
+);
+
+export default EducationItem;
