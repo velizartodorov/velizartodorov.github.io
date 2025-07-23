901df74af00c6b7db07d14c95766fe2633a36c11
velizartodorov <willking66@gmail.com>
2025-07-24 00:13:25 +0200

    :hammer: cleanup

--- src/components/common/utils.tsx
+++ src/components/common/utils.tsx
@@ -1,14 +1,6 @@
 import enCommon from './common.en.json';
 import nlCommon from './common.nl.json';
 
-export const HASSELT_REMOTE = 'Hasselt, Belgium (Remote)';
-export const GHENT_CONTRACT = 'Ghent, Belgium (Contract)';
-export const GHENT = 'Ghent, Belgium';
-export const RUSE = 'Rousse, Bulgaria';
-export const UNIVERSITY_RUSE = 'University of Rousse';
-export const VELIKO_TARNOVO = 'Veliko Tarnovo, Bulgaria';
-export const ELENA = 'Elena, Bulgaria';
-
 export const SERVICE = 'Service';
 export const PRODUCT = 'Product';
 export const CONSULTANCY = 'Consultancy';
