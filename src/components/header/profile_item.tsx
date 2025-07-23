efa69bbc342612c791fa5b93a72f8667d3ad061c
velizartodorov <willking66@gmail.com>
2025-07-23 02:22:12 +0200

    :hammer: refactor profile item

--- src/components/header/profile_item.tsx
+++ src/components/header/profile_item.tsx
@@ -3,16 +3,18 @@ import { Link } from '../header/link';
 import { getImageUrl } from '../common/utils';
 
 const ProfileItem: React.FC<{ link: Link }> = ({ link }) => {
+    const content = (
+        <>
+            <img src={getImageUrl(link.icon)} style={{ width: link.width }} alt={link.label} />
+            <span className="text-margin">{link.label}</span>
+        </>
+    );
     return link.url ? (
         <a className="no-underline" href={link.url} target="_blank" rel="noreferrer">
-            <img src={getImageUrl(link.icon)} style={{ width: link.width }} alt={link.label} />
-            <span className="text-margin">{link.label}</span>
+            {content}
         </a>
     ) : (
-        <span>
-            <img src={getImageUrl(link.icon)} style={{ width: link.width }} alt={link.label} />
-            <span className="text-margin">{link.label}</span>
-        </span>
+        <span>{content}</span>
     );
 };
 
