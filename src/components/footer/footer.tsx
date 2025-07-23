f5fbe575f4e5d98aa52e0c5acc3c23ac0716cfe1
velizartodorov <willking66@gmail.com>
2025-07-23 23:46:48 +0200

    :sparkles: make footer also multilangual

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,14 +3,17 @@ import { LanguageContext } from '../common/language_selector';
 import getProfile from './../header/profile.init';
 import './footer.css';
 import { useCurrentYear } from './utils';
+import enPeriod from '../common/lang.period.en.json';
+import nlPeriod from '../common/lang.period.nl.json';
 
 const Footer = () => {
   const { year } = useCurrentYear();
   const { language } = useContext(LanguageContext);
   const profile = getProfile(language);
+  const periodLang = language === 'nl' ? nlPeriod : enPeriod;
   return (
     <div className="text-center mt-2 mb-2 footer-font">
-      Powered by {profile.name} ® {year ?? ''} 😉 🚀
+      {periodLang.period.poweredBy} {profile.name} ® {year ?? ''} 😉 🚀
     </div>
   );
 };
