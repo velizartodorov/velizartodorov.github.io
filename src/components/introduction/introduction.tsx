2507b5381d0ab47b9c1aa9f6e220f0d5295896d9
velizartodorov <willking66@gmail.com>
2025-07-24 17:59:49 +0200

    :hammer: refactor introduction component

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -1,15 +1,11 @@
 import { useTranslation } from 'react-i18next';
 import AccordionWrapper from '../common/accordion_wrapper';
 import { Properties } from '../common/properties';
-import { interpolate, useIntroductionStats } from './utils';
+import { useFormatBody } from './utils';
 
 const Introduction = ({ className, eventKey }: Properties) => {
   const { t } = useTranslation();
-  const { totalTime, totalYears } = useIntroductionStats();
-  const bodyRaw = t('introduction:body', { returnObjects: true });
-  const body = Array.isArray(bodyRaw)
-    ? interpolate(bodyRaw.join(' '), { totalTime, totalYears })
-    : interpolate(String(bodyRaw), { totalTime, totalYears });
+  const body = useFormatBody(t('introduction:body', { returnObjects: true }));
   return (
     <AccordionWrapper
       title={t('introduction:title')}
