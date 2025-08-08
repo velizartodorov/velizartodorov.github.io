a812a6caf5f451cba925066023611db6c785a391
velizartodorov <willking66@gmail.com>
2025-08-08 17:26:46 +0200

    :art: adjust profile item label size

--- src/components/header/profile_item.tsx
+++ src/components/header/profile_item.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from '../header/link';
-
+import { Link } from './link';
+import './profile_item.css';
 
 const ProfileItem: React.FC<{ link: Link }> = ({ link }) => {
     const content = (
-        <>
+        <span className="profile-item-content">
             <img src={link.icon} style={{ width: link.width }} alt={link.label} />
             <span className="text-margin">{link.label}</span>
-        </>
+        </span>
     );
     return link.url ? (
         <a className="no-underline" href={link.url} target="_blank" rel="noreferrer">
