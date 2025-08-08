333aa852562ad5bbe9ce914fb3d1a5abbc9657b8
velizartodorov <willking66@gmail.com>
2025-08-08 16:42:23 +0200

    :art: fix accordion heading

--- src/components/common/accordion_wrapper.tsx
+++ src/components/common/accordion_wrapper.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Accordion, Card } from 'react-bootstrap';
+import './accordion_wrapper.css';
 import { Properties } from './properties';
 
 const AccordionWrapper: FC<Properties> = ({ title, children, className = '', eventKey = '0' }) => (
@@ -7,7 +8,7 @@ const AccordionWrapper: FC<Properties> = ({ title, children, className = '', eve
     <Accordion.Item eventKey={eventKey}>
       <Card>
         <Accordion.Header>
-          <h4 className="mb-1">{title}</h4>
+          <h4 className="mb-1 accordion-heading">{title}</h4>
         </Accordion.Header>
         <Accordion.Body>
           <Accordion>
