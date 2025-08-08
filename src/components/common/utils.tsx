6a7b387c939740680474b8c699e7b8e98f44efe7
velizartodorov <willking66@gmail.com>
2025-08-08 15:27:18 +0200

    :hammer: reorder functions in utils.tsx

--- src/components/common/utils.tsx
+++ src/components/common/utils.tsx
@@ -1,4 +1,15 @@
-import { TFunction } from "i18next";
+import { TFunction } from 'i18next';
+
+export function resolveDate(dateStr: string | undefined, t: TFunction): string {
+    if (!dateStr) return '';
+    const match = dateStr.match(/^\{\{\s*dates:([\w_-]+)\s*\}\}$/);
+    if (match) {
+        const key = match[1];
+        const value = t(key, { ns: 'dates' });
+        return value !== key ? value : '';
+    }
+    return dateStr;
+}
 
 export function parsePeriod(period: any, t?: TFunction) {
     if (t) {
@@ -13,20 +24,9 @@ export function parsePeriod(period: any, t?: TFunction) {
     };
 }
 
-export const bullet = '\u2022';
-
 export function currentDate(): Date {
     const now = new Date();
     return new Date(now.getFullYear(), now.getMonth(), now.getDate());
 }
 
-export function resolveDate(dateStr: string | undefined, t: TFunction): string {
-  if (!dateStr) return "";
-  const match = dateStr.match(/^\{\{\s*dates:([\w_-]+)\s*\}\}$/);
-  if (match) {
-    const key = match[1];
-    const value = t(key, { ns: "dates" });
-    return value !== key ? value : "";
-  }
-  return dateStr;
-}
+export const bullet = '\u2022';
