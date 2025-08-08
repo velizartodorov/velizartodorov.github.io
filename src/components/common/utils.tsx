2c9c1b24aca1bea54b1eeee361ded65b7e00a140
velizartodorov <willking66@gmail.com>
2025-08-08 14:36:05 +0200

    :hammer: refactor regex in utils.tsx

--- src/components/common/utils.tsx
+++ src/components/common/utils.tsx
@@ -22,7 +22,7 @@ export function currentDate(): Date {
 
 export function resolveDate(dateStr: string | undefined, t: TFunction): string {
   if (!dateStr) return "";
-  const match = dateStr.match(/^\{\{\s*dates:([\w_\-]+)\s*\}\}$/);
+  const match = dateStr.match(/^\{\{\s*dates:([\w_-]+)\s*\}\}$/);
   if (match) {
     const key = match[1];
     const value = t(key, { ns: "dates" });
