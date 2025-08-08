baca2382514f469e3afdfed8d07dca173f253c73
velizartodorov <willking66@gmail.com>
2025-08-08 12:53:01 +0200

    :bug: fix licenses certifications

--- src/components/licenses_certifications/licenses_certrifications.init.ts
+++ src/components/licenses_certifications/licenses_certrifications.init.ts
@@ -1,15 +1,17 @@
+import { useTranslation } from "react-i18next";
 import { LicenseCertification } from "./license_certification";
-import { useTranslation } from 'react-i18next';
 
 export function useLicensesCertifications(): LicenseCertification[] {
     const { t } = useTranslation();
-    const data = t('licenses:list', { returnObjects: true }) as any[];
-    return data.map((item: any) => ({
-        name: item.name ?? '',
-        institution: item.institution ?? '',
-        field: item.field ?? '',
-        date: item.date ? new Date(item.date) : new Date(0),
-        icon: item.icon ?? '',
-        link: item.link ?? '',
-    }));
+    const list = t('licenses_certifications:list', { returnObjects: true }) as any[];
+    return Array.isArray(list)
+        ? list.map((item: any) => ({
+           name: item.name ?? '',
+            institution: item.institution ?? '',
+            field: item.field ?? '',
+            date: item.date ? new Date(item.date) : new Date(0),
+            icon: item.icon ?? '',
+            link: item.link ?? '',
+        }))
+        : [];
 }
--- src/i18n.ts
+++ src/i18n.ts
@@ -3,14 +3,20 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 
-
 i18n
     .use(Backend)
     .use(initReactI18next)
     .init({
         lng: 'en',
         fallbackLng: 'en',
-        ns: ['common', 'employments', 'education', 'licenses', 'profile', 'introduction'],
+        ns: [
+            'common',
+            'employments',
+            'education',
+            'licenses_certifications',
+            'profile',
+            'introduction'
+        ],
         defaultNS: 'common',
         interpolation: {
             escapeValue: false,
