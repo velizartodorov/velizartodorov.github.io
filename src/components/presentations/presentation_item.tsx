bb1e3408895fb2d41f0c04c44541839829cebcc7
velizartodorov <willking66@gmail.com>
2025-09-02 13:57:27 +0200

    :art: adjust presentation item

--- src/components/presentations/presentation_item.tsx
+++ src/components/presentations/presentation_item.tsx
@@ -1,23 +1,16 @@
-
 import { FC } from 'react';
 import { Col, ListGroup, Row } from 'react-bootstrap';
 import { Presentation } from './presentation';
 
 const PresentationItem: FC<{ item: Presentation; index: number }> = ({ item }) => {
   return (
-    <ListGroup.Item
-      as='a'
+    <ListGroup.Item as='a'
       href={item.link}
       rel='noopener noreferrer'
-      className="p-10 list-group-item presentation-item"
-    >
+      className="p-10 list-group-item presentation-item">
       <Row className="align-items-center">
         <Col xs="auto" className="text-left presentation-icon">
-          <img
-            src={item.icon}
-            height="27"
-            alt="presentation icon"
-          />
+          <img src={item.icon} height="27" alt="presentation icon" />
         </Col>
         <Col xs={9} md={5} className="text-left">
           <h5 className="presentation-font mb-0">{item.name}</h5>
