f40501d62698b292acf58f84480f9efe2f3ba529
velizartodorov <willking66@gmail.com>
2026-06-18 14:41:06 +0200

    fix locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,4 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 type Language = 'en' | 'nl';
@@ -122,7 +121,7 @@ const loadTranslations = async () => {
 
 const initI18n = async () => {
   try {
-    i18n.use(Backend).use(initReactI18next);
+    i18n.use(initReactI18next);
     await i18n.init({
       lng: DEFAULT_LANGUAGE,
       fallbackLng: DEFAULT_LANGUAGE,
