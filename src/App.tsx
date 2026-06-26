85ad6cab154f90289ea00c4bafcfc831e04f5ef1
velizartodorov <willking66@gmail.com>
2026-06-26 18:57:42 +0200

    revert lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { lazy, Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import { LanguageSelector } from './components/common/language_selector';
+import Education from './components/education/education';
+import Employments from './components/employments/employments';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import Presentations from './components/presentations/presentations';
+import Languages from './components/languages/languages';
 import Introduction from './components/introduction/introduction';
 import { useProfile } from './components/header/profile.init';
+import LicensesCertifications from './components/licenses_certifications/licenses_certifications';
 import EnvBanner from './components/common/env_banner';
-
-const Employments = lazy(() => import('./components/employments/employments'));
-const LicensesCertifications = lazy(() => import('./components/licenses_certifications/licenses_certifications'));
-const Presentations = lazy(() => import('./components/presentations/presentations'));
-const Languages = lazy(() => import('./components/languages/languages'));
-const Education = lazy(() => import('./components/education/education'));
 
 globalThis.React = React;
 
@@ -32,14 +31,14 @@ export function App() {
         <Route
           path="/"
           element={
-            <Suspense fallback={null}>
+            <>
               <Introduction className="mx-4" eventKey="0" />
               <Employments className="mt-3 mx-4" eventKey="1" />
               <LicensesCertifications className="mt-3 mx-4" eventKey="2" />
               <Presentations className="mt-3 mx-4" eventKey="3" />
               <Languages className="mt-3 mx-4" eventKey="4" />
               <Education className="mt-3 mx-4" eventKey="5" />
-            </Suspense>
+            </>
           }
         />
         <Route path="*" element={<Navigate to="/" replace />} />
